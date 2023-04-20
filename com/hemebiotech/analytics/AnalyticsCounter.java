e458749edd699344fd3d7eda449ec95b39eb3ddd
Vanessa Roy <vl801w6nz3q3-student@anonymous-codio.com>
2023-04-20 17:34:56 +0000

    the correction of several errors

--- com/hemebiotech/analytics/AnalyticsCounter.java
+++ com/hemebiotech/analytics/AnalyticsCounter.java
@@ -5,39 +5,39 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;
+private static int headacheCount = 0;
 	private static int rashCount = 0;
-	private static int pupilCount = 0;
-	
+	private static int dialatedPupilCount = 0;
+
 	public static void main(String args[]) throws Exception {
 		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
+		FileReader fileReader = new FileReader("symptoms.txt");
+		BufferedReader reader = new BufferedReader(fileReader);
 		String line = reader.readLine();
 
-		int i = 0;
-		int headCount = 0;
 		while (line != null) {
-			i++;	// 
 			System.out.println("symptom from file: " + line);
 			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
+				headacheCount++;
+				System.out.println("number of headaches: " + headacheCount);
+			} else if (line.equals("rash")) {
 				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
+				System.out.println("number of rashes: " + rashCount);
+			} else if (line.contains("dialated pupils")) {
+				dialatedPupilCount++;
+				System.out.println("number of dialated pupils: " + dialatedPupilCount);
 			}
 
-			line = reader.readLine();	// get another symptom
+			line = reader.readLine(); // get another symptom
 		}
-		
+
+		reader.close();
+
 		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
+		FileWriter writer = new FileWriter("result.out", false); // replace to existent text in result.out
 		writer.write("headache: " + headacheCount + "\n");
 		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
+		writer.write("dialated pupils: " + dialatedPupilCount + "\n");
 		writer.close();
 	}
 }
