c8dcdf091ac30269935fb2cd28ef8b262fd58c98
Vanessa Roy <vl801w6nz3q3-student@anonymous-codio.com>
2023-05-09 09:12:00 +0000

    improvment to prevent leak ressources

--- com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -25,17 +25,25 @@ public class ReadSymptomDataFromFile implements ISymptomReader {
     ArrayList<String> result = new ArrayList<String>();
 
     if (filepath != null) {
+      BufferedReader reader = null;
       try {
-        BufferedReader reader = new BufferedReader(new FileReader(filepath));
+        reader = new BufferedReader(new FileReader(filepath));
         String line = reader.readLine();
 
         while (line != null) {
           result.add(line);
           line = reader.readLine();
         }
-        reader.close();
       } catch (IOException e) {
         e.printStackTrace();
+      } finally {
+        try {
+          if (reader != null) {
+            reader.close();
+          }
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
       }
     }
 
--- com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -9,14 +9,22 @@ public class WriteSymptomDataToFile implements ISymptomWriter {
 
   @Override
   public void writeSymptoms(Map<String, Integer> symptoms) {
+    FileWriter writer = null;
     try {
-      FileWriter writer = new FileWriter("result.out", false);
+      writer = new FileWriter("result.out", false);
       for (Entry<String, Integer> entry : symptoms.entrySet()) {
         writer.write(entry.getKey() + " : " + entry.getValue() + "\n");
       }
-      writer.close();
     } catch (IOException e) {
       e.printStackTrace();
+    } finally {
+      try {
+        if (writer != null) {
+          writer.close();
+        }
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
     }
   }
 }
